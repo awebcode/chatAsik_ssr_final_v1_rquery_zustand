42ef0d327c5f947897d42ca664063adce25ab7b4
Md-Asikur <asikurrahaman997@gamil.com>
2024-03-01 16:36:41 +0600

    c

--- app/[locale]/(chat)/conponents/chatHeader/ChatHeader.tsx
+++ app/[locale]/(chat)/conponents/chatHeader/ChatHeader.tsx
@@ -63,23 +63,20 @@ const ChatHeader = () => {
               <span className="text-xs ">
                 {isUserOnline ? (
                   <span className="text-green-500">Online</span>
+                ) : !isUserOnline &&
+                  !selectedChat.isGroupChat &&
+                  selectedChat?.userInfo?.lastActive ? (
+                  <span className="text-[9px]">
+                    active: {selectedChat?.userInfo?.lastActive as any}
+                    {moment(selectedChat?.userInfo?.lastActive as any)
+                      .startOf("hour")
+                      .fromNow()}
+                    ago
+                  </span>
                 ) : (
-                  <span className="text-rose-500">Offline</span>
+                  ""
                 )}
               </span>
-              {!isUserOnline &&
-              !selectedChat.isGroupChat &&
-              selectedChat?.userInfo?.lastActive ? (
-                <span className="text-[9px]">
-                  active: {selectedChat?.userInfo?.lastActive as any}
-                  {moment(selectedChat?.userInfo?.lastActive as any)
-                    .startOf("hour")
-                    .fromNow()}{" "}
-                  ago
-                </span>
-              ) : (
-                ""
-              )}
             </div>
           </>
         )}
